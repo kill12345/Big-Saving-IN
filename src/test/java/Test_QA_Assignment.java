4eeb56ac65830dd4a64f69ed9be18da070a7e51c
YOUR NAME <YOUR EMAIL ADDRESS>
2019-01-19 17:58:50 -0800

    Rejected commit: Default

--- src/test/java/Test_QA_Assignment.java
+++ src/test/java/Test_QA_Assignment.java
@@ -1,8 +1,6 @@
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 public class Test_QA_Assignment extends BaseClass {
 
@@ -15,53 +13,97 @@ public class Test_QA_Assignment extends BaseClass {
 
     public void firstTest() throws Exception {
 
-        Log_In log_in = new Log_In();
-        log_in.navigate_To_Page();
+        WebDriver driver = new ChromeDriver();
 
-        Create_Task create_task =new Create_Task();
-        WebDriver driver = new FirefoxDriver();
+        Main_Page log_in = new Main_Page();
+        log_in.navigate_To_Page_and_Chek_Title(driver);
 
-        // Verify all information on the application
+        log_in.isHome_Insurance_Present(driver);
 
-        assertTrue(create_task.isUserName_MassageDisplayed(driver));
-        assertTrue(create_task.isElement_MyTask_Present(driver));
-        assertEquals(create_task.isUserName_MassageDisplayed(driver),true);
+        log_in.isHomeInsuranceItemPresent(driver);
 
-        //Add and verify new task
+        log_in.isTwitterButtonPresent(driver);
 
-        create_task.setSomeCharacters(driver);
-        assertTrue(create_task.is_SomeCharactersPresent());
-        create_task.addNewTask();
+        log_in.isFacebookButtnPresentat(driver);
 
-         // Manage sub Task
+        log_in.isBigSavingInsuranceTextPresent(driver);
 
-        CreateSub_Tasks createSub_tasks = new CreateSub_Tasks();
-        assertTrue(createSub_tasks.isButton_Manage_Subtasks_Present(driver));
-        assertEquals(createSub_tasks.isButton_Manage_Subtasks_Present(driver),true);
-        createSub_tasks.clickButtonManageSubtasks();
+        log_in.getAlldropDownTypeOfInsurance(driver);
 
-        // Sub Tasks Form
-
-        createSub_tasks.waitForManageSubtasks_Form_ToBecomeVisible(1,driver);
-        assertEquals(createSub_tasks.is_ID_And_The_Task_Description_Present(driver),true);
-        createSub_tasks.setSomeSubTask_Description_249(driver);
-        createSub_tasks.setDateAndtime(driver);
-
-
-        // Verify Sub Tasks Form
-
-        assertEquals(createSub_tasks.getDateAndTime(),data);
-        assertEquals(createSub_tasks.getSomeTaskDescription(),notes);
-
-        assertTrue(createSub_tasks.isAddSubtaskButtonPresent(driver));
-        createSub_tasks.clickAddSubtaskButton(driver);
-
-
-        //Sub tasks that were added should be appended on the bottom part of the modal dialog
-
-        assertEquals(createSub_tasks.subtasksWereAdded(),notes);
+        log_in.celectedDropDownTypeOfInsurance(driver);
 
         driver.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+//
+//
+//        assertTrue(create_task.isUserName_MassageDisplayed(driver));
+//        assertTrue(create_task.isElement_MyTask_Present(driver));
+//        assertEquals(create_task.isUserName_MassageDisplayed(driver),true);
+//
+//        //Add and verify new task
+//
+//        create_task.setSomeCharacters(driver);
+//        assertTrue(create_task.is_SomeCharactersPresent());
+//        create_task.addNewTask();
+//
+//         // Manage sub Task
+//
+//        CreateSub_Tasks createSub_tasks = new CreateSub_Tasks();
+//        assertTrue(createSub_tasks.isButton_Manage_Subtasks_Present(driver));
+//        assertEquals(createSub_tasks.isButton_Manage_Subtasks_Present(driver),true);
+//        createSub_tasks.clickButtonManageSubtasks();
+//
+//        // Sub Tasks Form
+//
+//        createSub_tasks.waitForManageSubtasks_Form_ToBecomeVisible(1,driver);
+//        assertEquals(createSub_tasks.is_ID_And_The_Task_Description_Present(driver),true);
+//        createSub_tasks.setSomeSubTask_Description_249(driver);
+//        createSub_tasks.setDateAndtime(driver);
+//
+//
+//        // Verify Sub Tasks Form
+//
+//        assertEquals(createSub_tasks.getDateAndTime(),data);
+//        assertEquals(createSub_tasks.getSomeTaskDescription(),notes);
+//
+//        assertTrue(createSub_tasks.isAddSubtaskButtonPresent(driver));
+//        createSub_tasks.clickAddSubtaskButton(driver);
+//
+//
+//        //Sub tasks that were added should be appended on the bottom part of the modal dialog
+//
+//        assertEquals(createSub_tasks.subtasksWereAdded(),notes);
+
+
 
 
     }
